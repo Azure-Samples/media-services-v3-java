02a86bcce6793b909c4bf66d3f725e2b95d10c8b
v-jizhang <50673687+v-jizhang@users.noreply.github.com>
2019-08-15 14:11:08 -0700

    Update StreamHLSAndDASH.java

--- DynamicPackagingVODContent/StreamHLSAndDASH/src/main/java/sample/StreamHLSAndDASH.java
+++ DynamicPackagingVODContent/StreamHLSAndDASH/src/main/java/sample/StreamHLSAndDASH.java
@@ -110,7 +110,7 @@ public class StreamHLSAndDASH {
             // In this demo code, we will poll for Job status. Polling is not a recommended best practice for production
             // applications because of the latency it introduces. Overuse of this API may trigger throttling. Developers
             // should instead use Event Grid. To see how to implement the event grid, see the sample
-            // https://github.com/Azure-Samples/media-services-v3-dotnet/tree/master/ContentProtection/BasicAESClearKey.
+            // https://github.com/Azure-Samples/media-services-v3-java/tree/master/ContentProtection/BasicAESClearKey.
             job = waitForJobToFinish(manager, config.getResourceGroup(), config.getAccountName(), transform.name(),
                     jobName);
 
